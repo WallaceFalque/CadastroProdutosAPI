a04d1e07ba941442f3ae378e3e0beb2182f314a7
Wallace Falque <wallaceffalque@gmail.com>
2026-06-08 11:41:01 -0300

    Ajustes

--- Back/CadastroProdutos/obj/Debug/net10.0/CadastroProdutos.AssemblyInfo.cs
+++ Back/CadastroProdutos/obj/Debug/net10.0/CadastroProdutos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadastroProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dc83098833ebf351bdc788f651441c0b4a648e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadastroProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadastroProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
